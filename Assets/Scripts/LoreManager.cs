bdeabc0d934c4b33b09580ac9aebf5c4706196f8
AhmdFauzan <fauzan@aruvana.id>
2023-02-05 20:07:09 +0700

    add lore

---
+++ Assets/Scripts/LoreManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class LoreManager : MonoBehaviour
+{
+    [SerializeField]
+    private string[] lores;
+
+    [SerializeField] private TextMeshProUGUI loreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ShowLore());
+    }
+
+    private IEnumerator ShowLore()
+    {
+        Color textColor = loreText.color;
+
+        foreach(string str in lores)
+        {
+            loreText.text = str;
+
+            for(float i = 0; i <= 1; i = i + .05f)
+            {
+                textColor.a = i;
+                loreText.color = textColor;
+                yield return new WaitForSeconds(.05f);
+            }
+
+            for (float i = 1; i > 0; i = i - .05f)
+            {
+                textColor.a = i;
+                loreText.color = textColor;
+                yield return new WaitForSeconds(.05f);
+            }
+
+            textColor.a = 0;
+            loreText.color = textColor;
+
+            yield return new WaitForSeconds(.5f);
+        }
+
+        SceneManager.LoadScene("TitleScene");
+    }
+}
